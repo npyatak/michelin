859331c39a9aec6ec8c92c3c13b7c3aa2625af80
Mikhail <m.neznamov@yandex.ru>
2018-09-20 17:10:43 +0300

    Update video.php

--- frontend/views/site/video.php
+++ frontend/views/site/video.php
@@ -29,8 +29,11 @@
     		<?php $image = $v['image'] ? $v['image'] : "https://img.youtube.com/vi/".$v['id']."/maxresdefault.jpg";?>
 	        <div class="col-xs-offset-1 col-sm-offset-0 col-xs-10 col-sm-6 col-md-4 col-lg-4 contast-card"">
 	            <div class="contast-card-play-icon play-video" data-id="<?=$v['id']?>"></div>
-	            <div class="contest-image" style="background: url(<?=$image;?>) 0 0/cover;"></div>
-	            <div class="contest-text text_center"><?=$v['title'];?></div>
+	            <div class="contest-image" style="background: url(<?=$image;?>) 0 0/cover;">
+	            	<a href="" class="contest-link">Подробнее..</a>
+	            	<div class="contest-image-shadow"></div>
+	            </div>
+	            <div class="contest-text text_center">Снегоходы</div>
 	        </div>
     	<?php endforeach;?>
     </div>
