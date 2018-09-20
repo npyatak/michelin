cde6d4820ed6e8c556adc09814408ec7b03e03ce
Nick <pyatakn@gmail.com>
2018-09-20 09:41:05 +0300

    swipe

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,7 +18,7 @@ class AppAsset extends AssetBundle
         'css/menu.css?v=17.09_v2'
     ];
     public $js = [
-        'js/app.js?v=19.09_v2',
+        'js/app.js?v=20.09_v1',
         'js/jquery.slimscroll.js?v=16.09_v3',
         'js/main.js?v=19.09_v1',
         'js/menu.js?v=16.09_v1'
