a7b9dd2dd972f27f1372ca73f76e4682ffae9b11
Nick <pyatakn@gmail.com>
2018-09-26 11:36:53 +0300

    upd

--- frontend/views/site/video.php
+++ frontend/views/site/video.php
@@ -33,7 +33,7 @@ use yii\helpers\Url;
     		<?php $image = $v['image'] ? $v['image'] : "https://img.youtube.com/vi/".$v['id']."/maxresdefault.jpg";?>
 	        <div class="col-xs-offset-1 col-sm-offset-0 col-xs-10 col-sm-6 col-md-4 col-lg-4 contast-card"">
 	            <div class="contast-card-play-icon play-video" data-id="<?=$v['id']?>"></div>
-	            <div class="contest-image" style="background: url(<?=$image;?>) 0 0/cover;">
+	            <div class="contest-image" style="background: url(<?=Url::toRoute($image);?>) 0 0/cover;">
                     <?php if(isset($v['cityId'])):?>
 	            	    <a href="<?=Url::toRoute(['site/index', 'cityId' => $v['cityId']]);?>" class="contest-link">Подробнее..</a>
                     <?php endif;?>
