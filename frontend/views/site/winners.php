629945d276f143f119a764d7bfab7f1866d6ca7f
Petro <UWdevmail@gmail.com>
2018-10-01 20:26:58 +0300

    add button front

--- frontend/views/site/winners.php
+++ frontend/views/site/winners.php
@@ -1,13 +1,15 @@
 <?php $this->title = 'Лидеры викторины';?>
 
 <div class="page_wrapper">
-    <div class="row text_center">
-        <a href="#" class="main_title"></a>
-    </div>
+	<div class="container">
+	    <div class="row text-center">
+	        <a href="#" class="main_title"></a>
+	    </div>
+	</div>
 
 	<?php if($winners):?>
 		<div class="winners">
-			<h1 class="title text_center"><?=$this->title;?></h1>
+			<h1 class="title text-center"><?=$this->title;?></h1>
 			<?php foreach ($winners as $winner):?>
 				<div class="elem">
 					<div class="name"><?=$winner->user->fullName;?></div>
@@ -16,4 +18,5 @@
 			<?php endforeach;?>
 		</div>
 	<?php endif;?>
+	<div class="text-center mt-5"><a class="btn btn-oblique" href="/"><span>На главную</span></a></div>
 </div>
