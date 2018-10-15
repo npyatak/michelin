a7206dd97d1f5a3ce557addf63d0f56fbcf74b4e
feliksmardonkulov <fmardonkulov@gmail.com>
2018-10-15 23:51:28 +0600

    поделиться историей

--- frontend/views/creative-contest/index.php
+++ frontend/views/creative-contest/index.php
@@ -5,6 +5,55 @@ use yii\widgets\ListView;
 use kop\y2sp\ScrollPager;
 ?>
 
+<div class="page_wrapper history">
+    <div class="row text_center">
+        <a href="#" class="main_title"></a>
+    </div>
+
+    <div class="text text_center">
+        <div class="like-p">
+            <div><b>Расскажи</b> свою захватывающую историю путешествия</div>
+            <div>и <b>поделись</b> ею в социальных сетях</div>
+            <div><b>собирай</b> голоса и выигрывай новые</div>
+            <div>революционные шины <b>Michelin X-Ice North 4</b></div>
+        </div>
+    </div>
+    <div class="action">
+        <a class="btn btn-oblique btn-yellow" target="_blank" href=""><span>Поделиться историей</span></a>
+    </div>
+    <div class="gallery">
+        <div class="container">
+            <div class="item">
+                <div class="item-user" style="background-image: url('')">
+                    <span><span class="snow blue"></span>Иван Иванов</span>
+                    <span>1234</span>
+                </div>
+            </div>
+            <div class="item" style="background-image: url('')">
+                <div class="item-user">
+                    <span><span class="snow yellow"></span>Иван Иванов</span>
+                    <span>1234</span>
+                </div>
+            </div>
+            <div class="item" style="background-image: url('')">
+                <div class="item-user">
+                    <span><span class="snow yellow"></span>Иван Иванов</span>
+                    <span>1234</span>
+                </div>
+            </div>
+            <div class="item" style="background-image: url('')">
+                <div class="item-user">
+                    <span><span class="snow yellow"></span>Иван Иванов</span>
+                    <span>1234</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="more">
+        <a href="">Больше работ</a>
+    </div>
+</div>
+
 <?php if($dataProvider->models):?>
     <?= ListView::widget([
         'dataProvider' => $dataProvider,
--- frontend/views/creative-contest/participate.php
+++ frontend/views/creative-contest/participate.php
@@ -5,19 +5,52 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Поделиться историей';
 ?>
 
+<div class="confirm-upload">
+    <div class="confirm-title">Ссылка на историю</div>
+    <div class="confirm-link"><a href="">https://special.matchtv.spb.ru/michelin2018/123241325</a></div>
+    <a class="btn btn-oblique" target="_blank" href=""><span>ok</span></a>
+</div>
+
+<div class="upload-form">
+    <div class="upload-inner">
+        <div class="upload-title">Поделиться историей</div>
+        <form action="">
+            <div class="image"></div>
+            <div class="form-link">
+                <label for="form-link">или ссылка на видео на youtube</label>
+                <div>
+                    <input id="form-link" type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-text">
+                <label for="form-text">ваша история:</label>
+                <div>
+                    <textarea name="" id="form-text" class="form-control"></textarea>
+                </div>
+            </div>
+            <div class="form-bottom">
+                <div>История будет опубликована  в течение 24 часов</div>
+                <div>
+                    <button type="submit" class="btn btn-oblique btn-yellow"><span>Отправить</span></button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
 <?php $form = ActiveForm::begin([
     'id' => 'creative-contest-form',
     'options' => [
         'method' => 'post',
         'enctype' => 'multipart/form-data',
     ]
-    ]); ?>
+]); ?>
 
-    <?= $form->field($model, 'mediaFile')->fileInput() ?>
+<?= $form->field($model, 'mediaFile')->fileInput() ?>
 
-    <?= $form->field($model, 'yt_id')->textInput() ?>
+<?= $form->field($model, 'yt_id')->textInput() ?>
 
-    <?= $form->field($model, 'text')->textarea() ?>
+<?= $form->field($model, 'text')->textarea() ?>
 
     <div class="form-group">
         <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary', 'name' => 'creative-contest-button']) ?>
